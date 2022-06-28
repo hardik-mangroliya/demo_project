2f8a81d84c1190ae7d5e5b9768c3268f859ab291
hardik mangroliya <hardik.mangroliya@joflee.com>
2022-06-28 13:18:50 +0530

    delte some unused files.

--- lib/user_list.dart
+++ lib/user_list.dart
@@ -1,13 +1,9 @@
 import 'dart:async';
 import 'package:demo_project/core/dbHelper.dart';
-// import 'package:demo_project/module/home/view/controller/home_screenController.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-// import 'model/user_model.dart';
 import 'package:dio/dio.dart';
-
 import 'module/home/controller/home_screenController.dart';
 import 'module/home/model/user_model.dart';
 
@@ -17,7 +13,6 @@ class UserListScreen extends StatefulWidget {
     required this.user,
   }) : super(key: key);
 
-  // final SqliteModel sqliteModel;
   final User user;
 
   @override
